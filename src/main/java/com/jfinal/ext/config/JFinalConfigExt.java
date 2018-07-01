31f50d0f8f96679b3a1a90ab0b40b359404b5693
BruceZCQ <brucezcq@gmail.com>
2018-07-01 23:01:26 +0800

    update conf

--- src/main/java/com/jfinal/ext/config/JFinalConfigExt.java
+++ src/main/java/com/jfinal/ext/config/JFinalConfigExt.java
@@ -18,6 +18,8 @@ package com.jfinal.ext.config;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.apache.log4j.PropertyConfigurator;
+
 import com.alibaba.druid.filter.stat.StatFilter;
 import com.alibaba.druid.wall.WallFilter;
 import com.jfinal.config.Constants;
@@ -50,7 +52,7 @@ import com.jfinal.upload.OreillyCos;
  */
 public abstract class JFinalConfigExt extends com.jfinal.config.JFinalConfig {
 	
-	private final static String cfg = "jf-app-cfg.txt";
+	private final static String cfg = "conf/jf-app-cfg.conf";
 	
 	public static String APP_NAME = null;
 	protected boolean geRuned = false;
@@ -186,6 +188,7 @@ public abstract class JFinalConfigExt extends com.jfinal.config.JFinalConfig {
 	
 	public void afterJFinalStart() {
 		super.afterJFinalStart();
+		PropertyConfigurator.configure("conf/log4j.properties");
 		this.afterJFinalStarted();
 	}
 
