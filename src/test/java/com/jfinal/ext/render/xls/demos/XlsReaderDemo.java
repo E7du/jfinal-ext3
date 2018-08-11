c5b45072e096851266c0521103e8fc218bdc3229
BruceZCQ <brucezcq@gmail.com>
2018-08-11 14:23:06 +0800

    release 4.0.0

--- src/test/java/com/jfinal/ext/render/xls/demos/XlsReaderDemo.java
+++ src/test/java/com/jfinal/ext/render/xls/demos/XlsReaderDemo.java
@@ -12,29 +12,27 @@ import com.jfinal.kit.JsonKit;
 import com.test.api.model.User;
 
 class XlsReaderDemo {
-	
-	XlsReadRule xlsReadRule = null;
 
 	@Test
 	void test() {
 
-		xlsReadRule = new XlsReadRule();
+		XlsReadRule xlsReadRule = new XlsReadRule();
 		xlsReadRule.setStart(1);
 		xlsReadRule.setEnd(6);
 		xlsReadRule.setClazz(User.class);
-		
+
 		Column id = Column.create("id");
 		Column name = Column.create("name");
 		Column addr = Column.create("addr");
 		xlsReadRule.alignColumn(id, name, addr);
-		
-        String destFileName = "src/test/resources/users.xls";
-        
-       List<User> ret = XlsReader.readToModel(User.class, new File(destFileName), xlsReadRule);
-       User u = ret.get(0);
-      String json = JsonKit.toJson(ret);
-      System.out.println(json+"id="+u.getId()+";name="+u.getName()+";addr="+u.getAddr());
-        
+
+		String destFileName = "src/test/resources/users.xls";
+
+		List<User> ret = XlsReader.readToModel(User.class, new File(destFileName), xlsReadRule);
+		User u = ret.get(0);
+		String json = JsonKit.toJson(ret);
+		System.out.println(json + "id=" + u.getId() + ";name=" + u.getName() + ";addr=" + u.getAddr());
+
 	}
 
 }
